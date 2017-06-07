5f53e0ef86455b708d6f17301bc29a7a73f50c30
EnriqueNestozo <enrique.flores093@gmail.com>
2017-06-07 16:44:33 -0500

    Se termina la reservacion de la actividad

--- proyectoCADI/src/businesslogic/DAOClasses/ReservacionDAO.java
+++ proyectoCADI/src/businesslogic/DAOClasses/ReservacionDAO.java
@@ -1,8 +1,11 @@
 package businesslogic.DAOClasses;
+import businesslogic.Actividad;
+import businesslogic.Alumno;
 import java.util.List;
 import businesslogic.Reservacion;
 import dataaccess.ConexionBD;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -11,15 +14,27 @@ import java.util.ArrayList;
  * @author Enrique
  */
 public class ReservacionDAO extends ConexionBD {
-    public void crear(){
+    
+    public boolean crear(Alumno alumno,Actividad actividad){
+        boolean reservacionCreada = false;
         try {
             this.conectar();
-            PreparedStatement estado = this.conexion.prepareStatement("insert into reservacion values(?,?,?,?,?");
-            estado.setString(1, null);
+            java.util.Date fecha = new java.util.Date();
+            java.text.SimpleDateFormat formato = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String tiempoActual = formato.format(fecha);
+            PreparedStatement sentencia = this.conexion.prepareStatement("insert into reservacion values(?,?,?,?)");
+            sentencia.setString(1, null);
+            sentencia.setString(2, tiempoActual);
+            sentencia.setString(3, alumno.getMatricula());
+            sentencia.setString(4, actividad.getIdActividad());
+            if(sentencia.executeUpdate() == 1){
+                reservacionCreada = true;
+            }
             
         } catch (SQLException e) {
+            System.out.println("No se pudo hacer la reservacion" + e.getMessage());
         }
-        
+        return reservacionCreada;
     }
     
     public void eliminar(Reservacion reservacion){
@@ -31,11 +46,19 @@ public class ReservacionDAO extends ConexionBD {
         return listaDeReservacion;
     }
     
-    public boolean buscarReservacion(){
+    public boolean buscarReservacion(Alumno alumno, Actividad actividad){
         boolean yaReservada = false;
         try {
             this.conectar();
-            PreparedStatement estado = this.conexion.prepareStatement("select * from reservacion where idActividad = ?");
+            PreparedStatement sentencia = this.conexion.prepareStatement("select * from reservacion where matricula = ? AND idActividad = ?");
+            sentencia.setString(1, alumno.getMatricula());
+            sentencia.setString(2, actividad.getIdActividad());
+            ResultSet resultado = sentencia.executeQuery();
+            if(resultado.next()){
+                yaReservada = true;
+            }
+            this.cerrarConexion();
+            
         } catch (Exception e) {
         }
         return yaReservada;
